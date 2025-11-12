b03d54a074578d5afde508ecb3fb52b3ada4ed99
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2025-11-12 21:45:54 +0100

    Reuse exsting PV

--- kind/minio/install.sh
+++ kind/minio/install.sh
@@ -4,5 +4,8 @@ set -euxo pipefail
 cd $(dirname "$0")
 
 ../kubectl.sh apply -f persistentvolume.yaml
+if ../kubectl.sh get pv minio --no-headers | grep -qs "Released"; then
+  ../kubectl.sh patch pv minio -p '{"spec":{"claimRef": null}}'
+fi
 ../helm.sh upgrade --install minio --repo https://charts.min.io/ minio -n minio --values values.yaml
 ../kubectl.sh apply -k .
