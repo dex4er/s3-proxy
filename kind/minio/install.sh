4a7ae1e4334296db3d3740191ab9f36a90fff72d
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2025-11-12 21:37:41 +0100

    Static PV name

--- kind/minio/install.sh
+++ kind/minio/install.sh
@@ -3,5 +3,6 @@ set -euxo pipefail
 
 cd $(dirname "$0")
 
+../kubectl.sh apply -f persistentvolume.yaml
 ../helm.sh upgrade --install minio --repo https://charts.min.io/ minio -n minio --values values.yaml
 ../kubectl.sh apply -k .
